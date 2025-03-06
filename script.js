c001acfd3696ad682566362b7aeb80d9aa49b27c
mrromantic5 <ekgyasiarthur@gmail.com>
2025-03-06 06:23:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function checkPassword() {
-    let password = document.getElementById("password").value;
-    let errorMessage = document.getElementById("error-message");
-
-    if (password === "Romanticlove") {
-        document.getElementById("password-section").style.display = "none";  // Hide password section
-        document.getElementById("main-content").style.display = "flex";  // Show main content
-    } else {
-        errorMessage.textContent = "Incorrect password! Try again.";  // Show error message
-    }
-    }
+document.getElementById("viewBioBtn").addEventListener("click", function() {
+    document.getElementById("biography").scrollIntoView({ behavior: "smooth" });
+});
